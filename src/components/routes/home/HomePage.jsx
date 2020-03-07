b3531c4f4a38a1cf43be78b42291bbb94b7405bc
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-03-08 01:53:56 +0530

    Updated images, vscode settings

--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -4,9 +4,7 @@ import Button from 'components/ui/Button/Button';
 export default function HomePage() {
   return (
     <div>
-      <h1 style={{ fontSize: 50, fontWeigth: 'bold', textAlign: 'center' }}>
-        React Pages Boilerplate
-      </h1>
+      <h1 style={{ fontSize: 50, fontWeigth: 'bold', textAlign: 'center' }}>Hi Freeda</h1>
 
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Button theme="blue">Click me!!</Button>
